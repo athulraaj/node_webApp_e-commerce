66d1ab9f553d8532e99890865784987834d3e202
athulraaj <athuulraaj@gmail.com>
2025-08-20 23:24:33 +0530

    MongoDB connection created

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ dotenv.config();
 
 const app = express();
 
+//  Database connection
+const connectDB = require('./config/db');
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
